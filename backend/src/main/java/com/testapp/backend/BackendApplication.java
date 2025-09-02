c1612238d25d16a47f0b06e0831e5071d928222f
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-09-02 05:09:36 -0400

    chore: add cors config

--- backend/src/main/java/com/testapp/backend/BackendApplication.java
+++ backend/src/main/java/com/testapp/backend/BackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.testapp.backend.model.Movie;
 import com.testapp.backend.repository.MovieRepository;
@@ -15,7 +17,21 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	 @Bean
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                    .allowedOrigins("*")
+	                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                    .allowedHeaders("*")
+	                    .allowCredentials(false);
+	        }
+	    };
+	}
+
+	@Bean
     CommandLineRunner initDatabase(MovieRepository repository) {
         return args -> {
             repository.save(new Movie("The Shawshank Redemption", "Frank Darabont", 1994));
